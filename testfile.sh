d4b553224cd9bf698add03c6b3c57a3398aba07f
Clay Miller <clay@smockle.com>
2013-06-23 20:07:55 -0500

    Add Lim's testfile.

---
+++ testfile.sh
@@ -0,0 +1,53 @@
+# The following are the sequence of Linux commands 
+# with which you must test your codes for Project 1.
+# Submit your execution traces using the "script" command.
+
+ls -lsFd
+ls -lF
+who -as
+whoami --version
+ls ue*hv@ccy#ky^hbg%
+who ---h
+mkdir cse3500test
+mkdir -v
+cd cse3500
+mkdir -v misc
+ls -l
+hostname
+rmdir misc
+cd ..
+ls -lF
+mv cse3500test comp3500test
+chmod o-rx comp3500test
+ls -sFt
+print -l spectrum
+ifconfig
+netstat -r#&$(*^%
+whoami
+host
+arch
+h456Jg  HUG$$&## kjewb@#%% iweb^@*^#
+whoami eu34b^*@hj
+netstat -r
+chmod 700 comp3500test
+date
+echo there's no place like home
+cp -r comp3500test comp3500test-dup
+pwd -L
+more -p /etc/hosts
+man -k router
+chmod o-744 comp3500test-dup
+clear
+ifconfig
+kjherkerewwinrpijernkd ekeiifjeijfoijeoifjwn9@##$##  nicubriuy$%
+apropos ifconfig
+du -la
+df -ai
+ps ugx
+ps -aux test
+uptime
+w -u
+du who df
+rmdir comp3500test
+rmdir comp3500test-dup
+exit
